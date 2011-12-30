93933de899c97846a6eb8fa6c6ba0786fe51bbd4
Pete Browne <me@petebrowne.com>
2011-12-30 13:59:06 -0600

    Refactoring Compiler to follow the latest version in Rails

--- lib/guard/sprockets2/compiler.rb
+++ lib/guard/sprockets2/compiler.rb
@@ -1,8 +1,22 @@
 module Guard
   class Sprockets2::Compiler
+    attr_accessor :sprockets, :assets_path, :precompile, :digest, :gz
+    
     def initialize(options = {})
-      configure(options)
-      @target = Pathname.new(@assets_path)
+      if defined?(Rails) && Rails.application
+        @sprockets   = Rails.application.assets
+        @assets_path = File.join(Rails.public_path, Rails.application.config.assets.prefix)
+        @precompile  = Rails.application.config.assets.precompile
+      else
+        @assets_path = File.expand_path "public/assets"
+        @precompile  = [ Proc.new { |path| !%w(.js .css).include? File.extname(path) }, /(?:\/|\\|\A)application\.(css|js)$/ ]
+      end
+      @digest = true
+      @gz     = true
+      
+      options.each do |key, value|
+        send "#{key}=", value if respond_to? "#{key}="
+      end unless options.nil?
     end
   
     def clean
@@ -12,25 +26,22 @@ module Guard
     def compile
       @sprockets.send(:expire_index!)
       success = true
-      @precompile.each do |path|
-        @sprockets.each_logical_path do |logical_path|
-          next unless path_matches?(path, logical_path)
+      @sprockets.each_logical_path do |logical_path|
+        next unless compile_path?(logical_path)
 
-          if asset = @sprockets.find_asset(logical_path)
-            success = compile_asset(asset)
-            break unless success
-          end
+        if asset = @sprockets.find_asset(logical_path)
+          success = write_asset(asset)
+          break unless success
         end
       end
       success
     end
 
     protected
-
-    def compile_asset(asset)
-      filename = @digest ? @target.join(asset.digest_path) : @target.join(asset.logical_path)
-    
-      FileUtils.mkdir_p filename.dirname
+  
+    def write_asset(asset)
+      filename = File.join @assets_path, path_for(asset)
+      FileUtils.mkdir_p File.dirname(filename)
       asset.write_to(filename)
       asset.write_to("#{filename}.gz") if @gz && filename.to_s =~ /\.(css|js)$/
       true
@@ -39,35 +50,23 @@ module Guard
       UI.error e.message.gsub(/^Error: /, '')
       false
     end
-  
-    def path_matches?(path, logical_path)
-      if path.is_a?(Regexp)
-        path.match(logical_path)
-      else
-        File.fnmatch(path.to_s, logical_path)
+
+    def compile_path?(logical_path)
+      @precompile.each do |path|
+        case path
+        when Regexp
+          return true if path.match(logical_path)
+        when Proc
+          return true if path.call(logical_path)
+        else
+          return true if File.fnmatch(path.to_s, logical_path)
+        end
       end
+      false
     end
-  
-    def configure(options)
-      @sprockets = options[:sprockets]
-      @assets_path = options[:assets_path]
-      @precompile = options[:precompile]
-      @digest = options[:digest]
-      @gz = options[:gz]
-      set_defaults
-    end
-  
-    def set_defaults
-      if defined?(Rails)
-        @sprockets ||= Rails.application.assets
-        @assets_path ||= File.join(Rails.public_path, Rails.application.config.assets.prefix)
-        @precompile ||= Rails.application.config.assets.precompile
-      else
-        @assets_path ||= "#{Dir.pwd}/public/assets"
-        @precompile ||= [ /\w+\.(?!js|css).+/, /application.(css|js)$/ ]
-      end
-      @digest = true if @digest.nil?
-      @gz = true if @gz.nil?
+
+    def path_for(asset)
+      @digest ? asset.digest_path : asset.logical_path
     end
   end
 end
